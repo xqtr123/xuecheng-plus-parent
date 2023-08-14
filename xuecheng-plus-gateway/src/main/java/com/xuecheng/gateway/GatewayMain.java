a0c875e26fbb6482b3709fede4c3aa8bfefec0fb
Renshl <1581820822@qq.com>
2023-08-14 09:04:22 +0800

    完成网关的搭建

---
+++ xuecheng-plus-gateway/src/main/java/com/xuecheng/gateway/GatewayMain.java
@@ -0,0 +1,12 @@
+package com.xuecheng.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain.class, args);
+    }
+}
